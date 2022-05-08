9c849f195b435260b220262017d9b2894c37de1d
Tim Prapotnik <tim.prapotnik@student.um.si>
2022-05-08 16:28:12 +0200

    Added Infrastructure CORS

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventRouter = require('./routes/eventRoutes');
+var infrastructureRouter = require('./routes/infrastructureRoutes');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/event', eventRouter);
+app.use('/infrastructure', infrastructureRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
